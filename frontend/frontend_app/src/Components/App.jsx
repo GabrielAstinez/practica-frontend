8c1a4f595643f6d04478f9be0e815f0ea141cb73
Gabriel Astinez <finnCracK16@gmail.com>
2026-02-18 16:03:49 -0300

    Actualización: conexión frontend-backend y mejoras

--- frontend/frontend_app/src/Components/App.jsx
+++ frontend/frontend_app/src/Components/App.jsx
@@ -1,9 +1,11 @@
 import Playground from "./Playground";
 import Challenges from "./Challenges";
+import TaskDescriptions from "./TaskDescriptions";
 
 function App() {
   return (
     <div className="layout">
+      <TaskDescriptions />
       <Playground />
       <Challenges />
     </div>
---
+++ frontend/frontend_app/src/Components/TaskDescriptions.jsx
@@ -0,0 +1,29 @@
+function TaskDescriptions() {
+  return (
+    <div className="task-descriptions">
+      <h3>Descriptions Challenges</h3>
+      <div>
+        <h4>Challenge 1</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div>
+        <h4>Challenge 2</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div>
+        <h4>Challenge 3</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div>
+        <h4>Challenge 4</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div>
+        <h4>Challenge 5</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+  );
+}
+
+export default TaskDescriptions;
