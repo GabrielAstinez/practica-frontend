9d50d793ca8feffcb205b76dcb60df0227188c7a
Gabriel Astinez <finnCracK16@gmail.com>
2026-03-15 11:23:54 -0300

    Fix problema backend y mejora interfz

--- frontend/frontend_app/src/Components/TaskDescriptions.jsx
+++ frontend/frontend_app/src/Components/TaskDescriptions.jsx
@@ -7,6 +7,11 @@ function TaskDescriptions({ selectedChallenge }) {
         <div>
           <h4>{selectedChallenge.title}</h4>
           <p>{selectedChallenge.description}</p>
+
+          <h4>Expected result</h4>
+          <pre>
+            {JSON.stringify(selectedChallenge.expected_result, null, 2)}
+          </pre>
         </div>
       ) : (
         <p>Select a challenge</p>
